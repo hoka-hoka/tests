c930fc9010b72aec8dfbdca81aac3ef8867b54e1
semyonix_pokemonix <sff1@tpu.ru>
2021-02-08 12:18:14 +0700

    updated data

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,17 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import React, { useReducer } from 'react';
-import Sentence from './components/Sentence/Sentence';
-import Home from './pages/Home';
-import WordsProvider from './components/Sentence/WordsProvider/WordsProvider';
-import type { State, Action } from './App.types';
-
-function wordsReducer(state: State, action: Action): State {
-  switch (action.type) {
-    case 'SET_WORD_LIST':
-      return { ...state, wordMap: ([] as (HTMLElement | null)[]).concat(action.wordMap) };
-    case 'CAPTURE_WORD':
-      return { ...state, capture: action.capture, eventCapture: action.eventCapture };
-    case 'SET_START_COORDINATES':
-      return {
-        ...state,
-        coordsXY: ([] as Record<'x' | 'y', string | number>[]).concat(action.coordsXY),
-      };
-    default:
-      return state;
-  }
-}
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+// import Sentence from './components/Sentence/Sentence';
+// import Home from './pages/Home';
+// import WordsProvider from './components/Sentence/WordsProvider/WordsProvider';
+// import type { State, Action } from './App.types';
 
 function App() {
-  const [state, dispatch] = useReducer(wordsReducer, {
-    wordMap: [],
-    capture: false,
-    eventCapture: undefined,
-    coordsXY: [],
+  useEffect(() => {
+    fetch('https://mockend.com/hoka-hoka/tests/posts')
+      .then((response) => response.json)
+      .then((result) => console.log(result));
   });
-  return (
-    <BrowserRouter>
-      <WordsProvider>
-        <Sentence state={state} dispatch={dispatch} />
-      </WordsProvider>
-      <div>
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+  return <div>123</div>;
 }
 
 export default App;
